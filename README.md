db504a24585eaca4a25e864a6807c1d6cc37e313
juan carlos mordan <juan.mordan@gmail.com>
2017-05-18 08:38:03 -0400

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mark as Exclude Context Menu
+
+## Features
+
+* Menu: **Mark as excluded**: Add the selected file or folder to the exlude list in the ~/.vscode/settings.json file
+
+## Using
+
+* Right click on a folder or file in the Explorer, choose **Mark as excluded**
+
+![Preview](images/preview.png)
+
+## License
+
+[MIT](LICENSE.md)
---
+++ src/extension.ts
@@ -0,0 +1,55 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+  const filesExcludeKey = 'files.exclude';
+
+  function getUserConfigurationFilePath() {
+    return vscode.workspace.rootPath + '/.vscode/settings.json';
+  }
+
+  function getWorkspaceConfiguration() {
+    let configFile =  vscode.workspace.rootPath + '/.vscode/settings.json';
+    let jsonData = fs.readFileSync(configFile, { encoding: 'utf8' });
+    return JSON.parse(jsonData);
+  }
+
+  function saveConfiguration(config: any) {
+    let configFile = getUserConfigurationFilePath();
+    let jsonData = JSON.stringify(config, null, 2);
+    jsonData = jsonData.replace(/^[^{]+|[^}]+$/, '').replace(/(.+?[^:])\/\/.+$/gm, '$1');
+    fs.writeFileSync(configFile, jsonData, { encoding: 'utf8' });
+  }
+
+  // The explorer/context menu contribution receives the URI to the file/folder
+  let command = vscode.commands.registerCommand('extension.exclude', (e: vscode.Uri) => {
+    if(!e){
+      return;
+    }
+
+    let config = getWorkspaceConfiguration();
+    let elementToIgnore = '**' + e.path.substring(vscode.workspace.rootPath.length + 1);
+
+    if(!config[filesExcludeKey]){
+      config[filesExcludeKey] = {};
+    }
+    
+    if (!config[filesExcludeKey][elementToIgnore]) {
+      config[filesExcludeKey][elementToIgnore] = true;
+    }
+
+    saveConfiguration(config);
+  });
+
+  context.subscriptions.push(command);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
